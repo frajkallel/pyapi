a9690696d86156cb74b473f3d98f1146dc954c26
Fraj KALLEL <frajkallel@gmail.com>
2024-07-03 12:12:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, request, jsonify
 import psycopg2
 import os
 
+from azure.storage.blob import ContainerClient
+import json
+
 app = Flask(__name__)
 
 # Change the following values to match yours
@@ -11,12 +14,15 @@ db_password = os.getenv("db_password")
 db_host = os.getenv("db_host")
 db_port = os.getenv("db_port")
 
+conn_str = os.getenv("conn_str")
+container_name = os.getenv("container_name")
+
 # Establish a connection to the PostgreSQL database
 conn = psycopg2.connect(database=db_name, user=db_user, password=db_password,
                         host=db_host, port=db_port)
 cur = conn.cursor()
 
-# Define a route that accepts POST requests to run SQL queries
+# Define a route that accepts get requests to run SQL queries
 @app.route("/query", methods=["GET"])
 def query():
     cur.execute("select * from table1")
@@ -24,6 +30,16 @@ def query():
 
     # Return the result as JSON
     return jsonify(result)
+
+# Define a route that accepts get requests to list blob
+@app.route("/gfiles", methods=["GET"])
+def gfiles():
+    container = ContainerClient.from_connection_string(conn_str="my_connection_string", container_name="my_container")
+
+    blob_list = container.list_blobs()
+
+    # Return the result as JSON
+    return json.dumps(blob_list)
 
 # Run the Flask app on port 8080
 if __name__ == "__main__":
